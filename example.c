990ae81d4480db28df516f5c41f3786f2a45a494
yashwanth-mamilla <64597009+yashwanth-mamilla@users.noreply.github.com>
2022-03-15 22:48:43 +0530

    Add files via upload

--- example.c
+++ example.c
@@ -3,13 +3,31 @@
 int main()
 {
     int a;
-    int b;
+    int b,d;
     char c;
     a=20;
     c=45;
+   if(a > 19 ){
+    
+    	
+     //b = 1;
+    
+    }
+    else if(c == 45){
+        b = 2;
+    }
+    else {
+    
+        b = 2; 
+    	c+=1;
+    
+    }
+    
+   a = b+2;
 
-    a=b+1;
-    //printf("%d %d %d\n",a,b,c);
-
+   
+    // while(a!=30){
+    // 	a++;
+    // }
     return 0;
 }
